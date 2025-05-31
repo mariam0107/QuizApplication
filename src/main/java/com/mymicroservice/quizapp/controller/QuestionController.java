515acd37e948119c1bbcc0719afb291bd3588c5d
mariam george <mariam0107@gmail.com>
2025-05-31 12:27:03 +0200

    Added methods to get allquestions,questions by category,add new question.Added ResponseEntity to get Http status code

--- src/main/java/com/mymicroservice/quizapp/controller/QuestionController.java
+++ src/main/java/com/mymicroservice/quizapp/controller/QuestionController.java
@@ -3,9 +3,8 @@ package com.mymicroservice.quizapp.controller;
 import com.mymicroservice.quizapp.model.Question;
 import com.mymicroservice.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,7 +14,17 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
+
+    @GetMapping("category/{category}")
+        public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+            return questionService.getQuestionsByCategory(category);
+        }
+   @PostMapping("add")
+        public ResponseEntity<String> addQuestion(@RequestBody Question question){
+                return questionService.addQuestion(question);
+        }
+
 }
